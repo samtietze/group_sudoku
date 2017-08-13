235c2cc37f3f8bcdbd313ec486eb209c0aed7715
Sam Tietze <samtietze@gmail.com>
2017-08-12 20:06:34 -0500

    Write some baloney down

--- sudoku.rb
+++ sudoku.rb
@@ -20,3 +20,27 @@ end
 # form `solve` returns.
 def pretty_board(board)
 end
+
+# These next two methods are simply chaining events that
+# we want to occur first: they take in the puzzle's string
+# and make each element in to an integer (we can do math more
+# easily this way.):
+def board_string_splitter(board_string)
+  board_string.split('')
+end
+
+def make_it_an_integer(board_array)
+  board_array.map { |cell_item| cell_item.to_i }
+end
+
+#INPUT: cell key or row?? or the whole string?
+#OUTPUT: boolean
+def row_test(board_array)#???
+  # This test uses hash[X].keys[0][0] to find the row
+  # Here's a test case for repeated irb use:
+  #test = [{"111" => 1}, {"121" => "-"}, {"131" => 5}, {"141" => 8}, {"151" => "-"}, {"161" => 2}, {"171" => "-"}, {"181" => "-"}, {"191" => "-"}]
+  board_array.each do |check|
+    if check == (1..9)
+
+
+end
